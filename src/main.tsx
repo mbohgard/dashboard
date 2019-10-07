2a152aeb41b71c9d8b67b6ce3f1accdde5e7a3f3
Martin Bohgard <martin.bohgard@gmail.com>
2019-10-07 12:29:07 +0200

    Better error handling + logging

--- src/main.tsx
+++ src/main.tsx
@@ -71,6 +71,7 @@ const ErrorsContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+  border: solid 10px ${colors.red};
   background: rgba(0, 0, 0, 0.4);
 `;
 
--- src/server/services/transports.ts
+++ src/server/services/transports.ts
@@ -1,4 +1,4 @@
-import * as request from "request";
+import request from "request";
 
 import * as secrets from "../../../secrets";
 import * as config from "../../../config";
