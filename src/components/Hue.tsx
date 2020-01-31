303c3a1fdaa7855b457869487f2170dda95d3f46
Martin Bohgard <martin.bohgard@klarna.com>
2020-01-31 15:32:43 +0100

    Unwired slider in overlay on hue button long press

--- src/components/Hue.tsx
+++ src/components/Hue.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import tinycolor from "tinycolor2";
 
 import { useService } from "../hooks";
-import { def, percentageOfRange, limiter } from "../utils/helpers";
+import {
+  def,
+  percentageOfRange,
+  limiter,
+  memo,
+  debounce
+} from "../utils/helpers";
 
 import { bed, chair, child, computer, lamp, pot, sofa, hue } from "./Icon";
-import { ActionButton, DimmedIconBox as Box, ButtonGrid } from "./Atoms";
+import { DimmedIconBox as Box, ButtonGrid } from "./Atoms";
+import { ActionButton, Overlay } from "./Molecules";
+import { Range } from "./Range";
 
 const iconMap: { [key in GroupClass]?: JSX.Element } = {
   "Living room": sofa,
@@ -17,8 +25,33 @@ const iconMap: { [key in GroupClass]?: JSX.Element } = {
   Computer: computer
 };
 
+const getColor = memo((hue, sat, bri, ct) =>
+  def(hue, sat, bri)
+    ? tinycolor({
+        // hue in degrees
+        h: (hue! / 65535) * 360,
+        s: sat! / 254,
+        v: limiter(bri! / 254, 0.15)
+      }).toHexString()
+    : def(ct, bri)
+    ? tinycolor({
+        ...tinycolor
+          .mix(
+            tinycolor("#7ad3ff"),
+            tinycolor("#ffa500"),
+            percentageOfRange(153, 500)(ct!)
+          )
+          .toHsv(),
+        v: limiter(bri! / 254, 0.15)
+      }).toHexString()
+    : "#ddd"
+);
+
 export const Hue: React.FC = () => {
   const [groups, emit] = useService<HueServiceData>("hue", {});
+  const [[adjustId, adjustColor], setAdjustId] = useState<
+    [string, string] | []
+  >([]);
 
   const toggle = (id: string, action: HueEmitPayload) => {
     const set = emit({ id, ...action });
@@ -33,10 +66,30 @@ export const Hue: React.FC = () => {
       }));
   };
 
+  const adjust = useCallback(
+    debounce((value: number) => {
+      console.log(value);
+    }),
+    []
+  );
+
   return (
     <Box>
       {hue}
+      {adjustId && (
+        <Overlay closeOnPress close={() => setAdjustId([])}>
+          <Range color={adjustColor} onChange={adjust} initialValue={0} />
+        </Overlay>
+      )}
       <ButtonGrid>
+        <ActionButton
+          key="hej"
+          color="white"
+          active={false}
+          onLongPress={() => setAdjustId(["hej", "white"])}
+        >
+          {iconMap.Kitchen}
+        </ActionButton>
         {Object.keys(groups)
           .filter(k => Object.keys(iconMap).includes(groups[k].class))
           .map(k => {
@@ -44,25 +97,7 @@ export const Hue: React.FC = () => {
             const icon = iconMap[group.class];
 
             Array.isArray;
-            const color = def(hue, sat, bri)
-              ? tinycolor({
-                  // hue in degrees
-                  h: (hue! / 65535) * 360,
-                  s: sat! / 254,
-                  v: limiter(bri! / 254, 0.15)
-                }).toHexString()
-              : def(ct, bri)
-              ? tinycolor({
-                  ...tinycolor
-                    .mix(
-                      tinycolor("#7ad3ff"),
-                      tinycolor("#ffa500"),
-                      percentageOfRange(153, 500)(ct!)
-                    )
-                    .toHsv(),
-                  v: limiter(bri! / 254, 0.15)
-                }).toHexString()
-              : "#ddd";
+            const color = getColor(hue, sat, bri, ct);
 
             return (
               <ActionButton
@@ -70,7 +105,8 @@ export const Hue: React.FC = () => {
                 color={color}
                 active={group.on}
                 size={group.name === "Hjalmar" ? "48px" : undefined}
-                onClick={() => toggle(k, { on: !group.on })}
+                onPress={() => toggle(k, { on: !group.on })}
+                onLongPress={() => setAdjustId([k, color])}
               >
                 {icon}
               </ActionButton>
