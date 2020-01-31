9ea006e741d57eb124fdb4e4c67abf1e5ad9e0ee
Martin Bohgard <martin.bohgard@klarna.com>
2020-01-31 15:31:04 +0100

    Performance improvements and helpers + overlay component

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -15,3 +15,53 @@ export const parse = (input: any) => {
     return input;
   }
 };
+
+type Obj = { [s: string]: any };
+
+export const areEqual = (a: Obj, b: Obj) => {
+  for (const k in a) {
+    if (a[k] !== b[k]) return false;
+  }
+
+  return true;
+};
+
+export const memo = <F extends (...args: any[]) => any>(func: F) => {
+  const cache = new Map();
+
+  const set = (args: any, str: string) => {
+    const val = func.apply(null, args);
+
+    cache.set(str, val);
+
+    return val;
+  };
+
+  return (...args: Parameters<F>): ReturnType<F> => {
+    if (cache.size > 1000) cache.clear();
+
+    const str = args.join();
+
+    return cache.get(str) || set(args, str);
+  };
+};
+
+export const debounce = <F extends (...args: any[]) => any>(
+  f: F,
+  wait = 300
+) => {
+  let t: number;
+
+  return (...args: Parameters<F>): Promise<ReturnType<F>> =>
+    new Promise((resolve, reject) => {
+      clearTimeout(t);
+
+      t = setTimeout(() => {
+        try {
+          resolve(f(...args));
+        } catch (error) {
+          reject(error);
+        }
+      }, wait);
+    });
+};
